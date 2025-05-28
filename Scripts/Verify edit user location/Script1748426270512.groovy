9f5ba5dd8985069172671bf96bbf6d4f423d951f
rifkykurniawan <toettoet.tt@gmail.com>
2025-05-28 17:12:02 +0700

    First commit

---
+++ Scripts/Verify edit user location/Script1748426270512.groovy
@@ -0,0 +1,43 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+
+String finalName = 'Rifky ' + System.currentTimeMillis()
+
+def words = ['Yogyakarta', 'Magelang', 'Surabaya', 'Malang', 'Jakarta']
+
+def randomWord = words[new Random().nextInt(words.size())]
+
+Mobile.startExistingApplication('com.mufumbo.android.recipe.search')
+
+Mobile.verifyElementVisible(findTestObject('android.widget.TextView - Search'), 0)
+
+Mobile.tapAtPosition(100, 260)
+
+Mobile.tap(findTestObject('Object Repository/android.view.View (1)'), 0)
+
+Mobile.tap(findTestObject('Object Repository/android.widget.Button - Edit Profile'), 0)
+
+Mobile.setText(findTestObject('Edit Profile - Location'), randomWord, 0)
+
+Mobile.tap(findTestObject('Object Repository/android.widget.Button - Update'), 0)
+
+Mobile.verifyElementText(findTestObject('Profile Page - Location'), randomWord)
+
+Mobile.closeApplication()
+
---
+++ Test Suites/Cookpad App.ts
@@ -0,0 +1,28 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Cookpad App</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>0155b08c-4161-4be8-8e0a-ce3eb6aff9b6</testSuiteGuid>
+   <testCaseLink>
+      <guid>80fdcd9f-7db3-47e9-aaa6-3aef52080f11</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Verify edit user location</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>235d1ece-13a5-4041-94a3-8e5d26a92774</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Verify edit user display name</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
